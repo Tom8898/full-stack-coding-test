41621c5d3bc82c663b3cde171973e3aaf5b78f66
Tom8898 <jingtao416@gmail.com>
2026-06-19 11:36:39 +1200

    add API test for /user/:id endpoint

--- backend/test/api/users.spec.ts
+++ backend/test/api/users.spec.ts
@@ -18,3 +18,27 @@ describe('/users', () => {
         expect((response.body[0] as User).id).toBeDefined();
     });
 });
+
+describe('/user/:id', ()=> {
+    it('should return a user based on a valid id parameter', async () => {
+        const response = await request(app).get('/user/99a8024b-f212-49ce-bc54-ee310c0e0007');
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toHaveProperty('id', '99a8024b-f212-49ce-bc54-ee310c0e0007');
+    });
+
+    it('should return 400 for a invalid parameter', async () => {
+        const response = await request(app).get('/user/%20');
+
+        expect(response.status).toEqual(400);
+    });
+
+    it('should return 404 if user is not found', async () => {
+        const response = await request(app).get('/user/1');
+
+        expect(response.status).toEqual(404);
+    });
+
+
+
+});
